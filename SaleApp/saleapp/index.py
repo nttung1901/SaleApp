83685f68d2dc42d5555e10a41252f0aee43ffb4f
nttung1901@gmail.com <nttung1901@gmail.com>
2024-11-15 16:40:55 +0700

    buoi3

--- SaleApp/saleapp/index.py
+++ SaleApp/saleapp/index.py
@@ -1,4 +1,4 @@
-from flask import render_template,request
+from flask import render_template,request,redirect
 
 import dao
 from saleapp import app
@@ -15,6 +15,15 @@ def index():
 def details(id):
     product = dao.load_product_by_id(id)
     return render_template('product-details.html',product=product)
+
+@app.route('/login', methods=['get','post'])
+def login_my_user():
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username.__eq__('admin') and password.__eq__('123'):
+            return redirect('/')
+    return render_template('login.html')
 
 @app.context_processor
 def common_attributes():
--- SaleApp/saleapp/model.py
+++ SaleApp/saleapp/model.py
@@ -1,6 +1,17 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,Boolean,Float,ForeignKey
 from saleapp import app, db
 
+
+class User(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100))
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50),nullable=False)
+    avatar = Column(String(200),default="https://res.cloudinary.com/dqe19i7og/image/upload/v1731659931/pngtree-account-avatar-user-abstract-circle-background-flat-color-icon-png-image_1650938_xqovwm.jpg")
+    active = Column(Boolean = True, nullable=False)
+
+    def __str__(self):
+        return self.name
 
 class Category(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -10,9 +21,21 @@ class Category(db.Model):
         return self.name
 
 
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100))
+    price = Column(Float,default=0)
+    image = Column(String(200), default="https://res.cloudinary.com/dqe19i7og/image/upload/v1730955526/apple-iphone-16-ultramarine_jj1lxj.webp")
+    category_id= Column()
+
+    def __str__(self):
+        return self.name
+
 if __name__ == "__main__":
     with app.app_context():
-        #db.create_all()
-        c = Category(name = "Mobile")
-        db.session.add(c)
-        db.session.commit()
+        db.create_all()
+        # c1 = Category(name = "Mobile")
+        # c2 = Category(name = "Laptop")
+        # c3 = Category(name = "Tablet")
+        # db.session.add_all([c1,c2,c3])
+        # db.session.commit()
